f990aabfb264e9db5f3312bec9f2832bb9f8c390
svorbrugg <sebastian.vorbrugg@student.uni-tuebingen.de>
2022-12-12 10:58:08 +0100

    Map -> for_each (now working) + added test

--- benches/bench.rs
+++ benches/bench.rs
@@ -3,7 +3,8 @@ use rand::prelude::SliceRandom;
 use rand::thread_rng;
 use group_sort::{group_sort};
 
-/// Size * 20 vector (shuffled)
+/// Create new vector - size * 20 (shuffled)
+///
 pub fn make_data(size: usize) -> Vec<usize>{
     let mut ff = Vec::new();
     for x in 0..size {
@@ -15,25 +16,31 @@ pub fn make_data(size: usize) -> Vec<usize>{
     return ff
 }
 
+/// Group_sort (+ make new data)
 fn new_sort(size: usize){
     let mut intervals = make_data(size);
     //sort_vector(&mut intervals);
     let mut network = group_sort(intervals);
 }
 
+/// Default sorting (+ make data)
 fn old_sort(size: usize){
     let mut intervals = make_data(size);
     //sort_vector(&mut intervals);
     let mut network = intervals.sort();
 }
 
+/// Make data (as comparison)
 fn data(size: usize){
     let mut intervals = make_data(size);
     //sort_vector(&mut intervals);
 }
 
+
+/// Benchmark with criterion
 fn criterion_benchmark(c: &mut Criterion) {
     let f = 10000;
+    println!("Benches run with vector of size 200000");
     c.bench_function("New implementation", |b| b.iter(|| new_sort(f)));
     c.bench_function("Default implementation", |b| b.iter(|| old_sort(f)));
     c.bench_function("Data generation solo", |b| b.iter(|| data(f)));
--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,22 @@
 pub fn group_sort(input: Vec<usize>) -> Vec<usize>{
     let max_val = input.iter().max().unwrap();
     let mut steps = vec![0; *max_val+1];
-    input.iter().map(|y| { steps[*y] += 1});
+    input.iter().for_each(|y| { steps[*y] += 1});
 
     let cstep_index = cum_step_index(steps);
 
     let mut update = vec![0; *max_val+1];
     let mut result = vec![0; input.len()];
-    input.into_iter().map(|x| { result[cstep_index[x] + update[x]] = x; update[x] += 1});
+
+    input.into_iter().for_each(|x| { result[cstep_index[x] + update[x]] = x; update[x] += 1});
+    //input.into_iter().map(|x| { result[cstep_index[x] + update[x]] = x; update[x] += 1});
     return result
 }
 
-/// Step index
+/// Cummultive step index
 ///
 /// Step index -> cum step index
+/// Biggest number -> Length of the sequential data (group_sort input)
 pub fn cum_step_index(v: Vec<usize>) -> Vec<usize>{
     let mut last_index = 0;
     let mut step_ind: Vec<usize> = v.iter().map(|x|{
@@ -40,37 +43,51 @@ mod tests {
     use rand::seq::SliceRandom;
     use super::*;
 
-    /// Shuffled
-    pub fn make_data(u: usize) -> Vec<usize>{
+    /// Create new vector - size * 20 (shuffled)
+    ///
+    pub fn make_data(size: usize) -> Vec<usize>{
         let mut ff = Vec::new();
-        for x in 0..u{
-            for _ in 0..20{
+        for y in 0..size {
+            for x in 0..20{
                 ff.push(x)
             }
         }
-        ff.shuffle(&mut thread_rng());
         return ff
     }
 
+
+    #[test]
+    fn test_genera() {
+        let result = vec![1,2,3,3,3,4,1,1,5,5,6,7,6,2,3];
+
+
+        let i = group_sort(result);
+        let mut result = vec![1,2,3,3,3,4,1,1,5,5,6,7,6,2,3];
+        result.sort();
+        assert_eq!(i, result);
+    }
 
 
     #[test]
     fn testing_big() {
         use std::time::Instant;
         let result = make_data(10000);
-        println!("New");
+
+        println!("Running tests: size 100000*20\n");
+        println!("Group sorting");
         let start = Instant::now();
 
         group_sort(result);
         let elapsed = start.elapsed();
         println!("Millis: {} ms", elapsed.as_millis());
         let mut result = make_data(10000);
-        println!("Old");
+        println!();
+        println!("Default sorting ");
         let start = Instant::now();
-
         result.sort();
         let elapsed = start.elapsed();
         println!("Millis: {} ms", elapsed.as_millis());
-
+        println!("DONE");
+        println!();
     }
 }
